14aa4eab23f8a2950ff54211c751c41fc5d6d060
Patrick Clancey <patrick.clancey@cognifide.com>
2016-06-21 17:02:52 +0100

    gulp refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,55 @@
 var gulp = require('gulp'),
-		browserSync = require('browser-sync'),
-		reload = browserSync.reload,
-		$ = require('gulp-load-plugins')();
+	autoprefixer = require('gulp-autoprefixer'),
+	browserSync = require('browser-sync'),
+	combineMq = require('gulp-combine-mq'),
+	plumber = require('gulp-plumber'),
+	print = require('gulp-print'),
+	reload = browserSync.reload,
+	sass = require('gulp-sass'),
+	$ = require('gulp-load-plugins')();
 
 // Define paths
 var paths = {
-	srcCSS: './src/styles/',
-	distCSS: './build/styles/',
 	srcJS : './src/scripts/' ,
 	distJS: './build/scripts/',
-	srcI: './src/images/',
-	distI: './build/images/',
-	srcT: './src/templates/',
-	distT: './build/'
-}
+	srcCSS : './src/sass/' ,
+	distCSS: './build/css/'
+};
 
-// Setup PostCSS Plugins
-var processors = [
-	require('cssnext')(), // https://github.com/cssnext/cssnext
-	require('postcss-mixins')(), // https://github.com/postcss/postcss-mixins
-	require('postcss-simple-vars')(), //https://github.com/postcss/postcss-simple-vars
-	require('postcss-nested')(), //https://github.com/postcss/postcss-nested
-	require('postcss-import')({
-			path: [paths.srcCSS]
-		}), // https://github.com/postcss/postcss-import
-	require('postcss-pseudo-class-enter')(), //https://github.com/jonathantneal/postcss-pseudo-class-enter
-	require('postcss-position')(), // https://github.com/seaneking/postcss-position
-	require('postcss-size'), // https://github.com/postcss/postcss-size
-	require('postcss-quantity-queries')(), // https://github.com/pascalduez/postcss-quantity-queries
-	require('autoprefixer-core')({ browsers: ['last 2 version'] }), // https://github.com/postcss/autoprefixer-core
-	require('postcss-reporter')() // https://github.com/postcss/postcss-reporter
+var browsersTable = [
+	'chrome >= 40',
+	'firefox >= 30',
+	'safari >= 7',
+	'ie >= 9',
+	'ios_saf >= 6',
+	'and_chr >= 40',
+	'and_uc >= 4'
 ];
 
-gulp.task('styles', function () {
-	return gulp.src(paths.srcCSS + 'style.css')
-		.pipe($.plumber({
-		  errorHandler: $.notify.onError("Error: <%= error.message %>")
-		}))
-		.pipe($.sourcemaps.init())
-		.pipe($.postcss(processors))
-		.pipe($.minifyCss())
-    .pipe($.sourcemaps.write())
-		.pipe(gulp.dest(paths.distCSS))
-		.pipe(reload({stream:true}));
-});
+// Convenience handler for error-level errors.
+function onError(error) { handleError.call(this, 'error', error);}
+// Convenience handler for warning-level errors.
+function onWarning(error) { handleError.call(this, 'warning', error);}
 
-gulp.task('templates', function () {
-	return gulp.src([paths.srcT + '*.jade', '!' + paths.srcT + 'layout.jade'])
-		.pipe($.plumber({
-		  errorHandler: $.notify.onError("Error: <%= error.message %>")
+
+gulp.task('styles', function(cb) {
+	console.log('[gulp] Compiling sass: ');
+	return gulp
+		.src(paths.srcCSS + '*.scss')
+		.pipe(plumber({
+			errorHandler: onError
 		}))
-		.pipe($.jade({
-	    pretty : true
-	  }))
-		.pipe(gulp.dest(paths.distT))
-		.pipe(reload({stream:true}));
-})
+		.pipe(sass())
+		.pipe(autoprefixer({browsers: browsersTable}))
+		.pipe(combineMq({
+			beautify: true
+		}))
+		.pipe(gulp.dest(paths.distCSS))
+		.pipe(reload({stream:true}))
+		.pipe(print(function(filepath) {
+			return "CSS generated: " + filepath;
+		}));
+});
 
 gulp.task('scripts', function () {
 	return gulp.src(paths.srcJS + '*.js')
@@ -65,7 +59,6 @@ gulp.task('scripts', function () {
 		.pipe($.sourcemaps.init())
 		.pipe($.babel())
 		.pipe($.concat('main.min.js'))
-		// .pipe($.stripDebug())
 		.pipe($.uglify())
 		.pipe($.sourcemaps.write('.'))
 		.pipe(gulp.dest(paths.distJS))
@@ -73,10 +66,8 @@ gulp.task('scripts', function () {
 });	
 
 gulp.task('watch', function() {
-	gulp.watch(paths.srcCSS + '**/*.css', ['styles']);
 	gulp.watch(paths.srcJS + '**/*.js', ['scripts']);
-	gulp.watch(paths.srcT + '**/*.jade', ['templates']);
-	gulp.watch(paths.srcI, ['images']);
+	gulp.watch(paths.srcCSS + '**/*.scss', ['styles']);
 });
 
 gulp.task('browser-sync', function () {
@@ -87,10 +78,5 @@ gulp.task('browser-sync', function () {
 	})
 });
 
-gulp.task('images', function () {
-	return gulp.src(paths.srcI + '*')
-		.pipe($.imagemin())
-		.pipe(gulp.dest(paths.distI));
-});
 
-gulp.task('default', ['styles', 'templates', 'scripts', 'images', 'browser-sync','watch']);
+gulp.task('default', ['styles','scripts','browser-sync','watch']);
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,173 +1,346 @@
-const Form = React.createClass({
+
+
+const App = React.createClass({
 	getInitialState() {
+		let mod = 5;
 		return {
-			mod: 3
+			items: Array.apply(null, Array(mod)).map(function () {}),
+			mod: mod
 		}
+	},
+	setItems(mod){
+		var arr = Array.apply(null, Array(mod)).map(function () {});
+		this.setState({items:arr});
 	},
 	updateMod(e) {
-		this.setState({mod: e.target.value});
-		this.props.onUpdate(e.target.value)
-	},
-	displayAmount(e) {
-		this.setState({value: e.target.value});
-	},
-	render() {
-		return (
-			<section className="formControl">
-				<h2>Build a query</h2>
-				<form>
-					<div className="inputRow">
-						<label htmlFor="element" className="inputLabel">Mod {this.state.mod}</label>
-						<input data-tag={this.state.mod}
-							   type="range"
-							   step="1"
-							   min="2"
-							   max="10"
-							   onChange={this.updateMod}
-							   value={this.state.mod}
-							   autoFocus ref="mod" required  />
-					</div>
-				</form>
-			</section>
-		);
-	}
-});
-
-const Example = React.createClass({
-	getInitialState() {
-		return {
-			items: ['i', 'i', 'i', 'i', 'i']
-		}
+		let mod = parseInt(e.target.value),
+			arr = Array.apply(null, Array(mod)).map(function () {});
+		this.setState({
+			mod: mod,
+			items: arr
+		});
 	},
 	addItem() {
-		let newArray = this.state.items;
-		newArray.push("i");
-		this.setState({arr:newArray});
+		let arr = this.state.items;
+		arr.push("");
+		this.setState({items:arr});
 	},
 	removeItem() {
 		if (this.state.items.length > 1) {
-			let newArray = this.state.items;
-			newArray.pop();
-			this.setState({arr:newArray})
+			let arr = this.state.items;
+			arr.pop();
+			this.setState({items:arr})
 		} else {
 			return
 		}
 	},
+	getWidth(i){
+		return Math.round(100000/i)/1000+"%"
+	},
+	makeCommonCSS(){
+		return `/* common to all queries */
+li:first-child {
+	width: 100%;
+}
+li:nth-child(2):last-child {
+	margin-left: 25%;
+	width: 50%;
+}`;
+	},
+	makeSecondRowCSS(mod) {
+		let styles = `\n/* styles for less than ${mod} items */`,i=2;
+		for (;i<mod;i++){
+			styles+= `\nli:nth-last-child(${i+1}):first-child ~ li { width: ${this.getWidth(i)} }`;
+		}
+		return styles;
+	},
+	makeModCSS(mod){
+		let query = `\n/* mod query ${mod} */\nli {
+	width: ${this.getWidth(mod)};
+}
+li:nth-last-child(${mod}n+0):first-child ~ li:nth-child(n+2):nth-child(-n+${mod}) {
+	width: ${this.getWidth(mod-1)};
+}
+li:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+${mod+2}) {
+	width: ${this.getWidth(mod-1)};
+}
+li:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+	width: ${this.getWidth(2)};
+}`;
+	//	if ( mod === 3 ){
+
+//			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+${mod+2}) {
+//	width: ${this.getWidth(mod-1)};
+//}`;
+//			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+${mod-1}) {
+//	width: ${this.getWidth(mod-2)};
+//}`;
+	//}
+//		query+="/*  this is the original bit */";
+//		query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+//	width: ${this.getWidth(2)};
+//}`;
+		if (mod > 3) {
+			query += "\n/*  this is the dynamic bit */";
+			for (let i = 3; i < mod; i++) {
+				query += `\nli:nth-last-child(${mod}n+${i}):first-child ~ li:nth-child(n+2):nth-child(-n+${i}) {
+	width: ${this.getWidth(i - 1)};
+}`;
+			}
+		}
+//		if ( mod === 14 ){
+//			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+//	width: ${this.getWidth(2)};
+//}`;
+//			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+4):nth-child(-n+6) {
+//	width: ${this.getWidth(3)};
+//}`;
+// 			query+= `\nli:nth-last-child(${mod}n+3):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+// 		}
+// 		if ( mod === 15 ){
+// 		query+="/*  this is the original bit */";
+// 			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+//			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+4):nth-child(-n+7) {
+//	width: ${this.getWidth(4)};
+//}`;
+//			query+= `\nli:nth-last-child(${mod}n+3):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+//	width: ${this.getWidth(2)};
+//}`;
+//			query+= `\nli:nth-last-child(${mod}n+4):first-child ~ li:nth-child(n+2):nth-child(-n+4) {
+//	width: ${this.getWidth(3)};
+//}`;
+//		}
+//		if ( mod === 16 ){
+//			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+//	width: ${this.getWidth(2)};
+//}`;
+//			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+4):nth-child(-n+8) {
+//	width: ${this.getWidth(5)};
+//}`;
+// 			query+= `\nli:nth-last-child(${mod}n+3):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+4):first-child ~ li:nth-child(n+2):nth-child(-n+4) {
+// 	width: ${this.getWidth(3)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+5):first-child ~ li:nth-child(n+2):nth-child(-n+5) {
+// 	width: ${this.getWidth(4)};
+// }`;
+// 		}
+// 		if ( mod === 7 ){
+// 			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+// //			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+4):nth-child(-n+9) {
+// //	width: ${this.getWidth(6)};
+// //}`;
+// 			query+= `\nli:nth-last-child(${mod}n+3):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+4):first-child ~ li:nth-child(n+2):nth-child(-n+4) {
+// 	width: ${this.getWidth(3)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+5):first-child ~ li:nth-child(n+2):nth-child(-n+5) {
+// 	width: ${this.getWidth(4)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+6):first-child ~ li:nth-child(n+2):nth-child(-n+6) {
+// 	width: ${this.getWidth(5)};
+// }`;
+// 		}
+// 		if ( mod === 8 ){
+// 			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+// //			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+4):nth-child(-n+10) {
+// //	width: ${this.getWidth(7)};
+// //}`;
+// 			query+= `\nli:nth-last-child(${mod}n+3):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+4):first-child ~ li:nth-child(n+2):nth-child(-n+4) {
+// 	width: ${this.getWidth(3)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+5):first-child ~ li:nth-child(n+2):nth-child(-n+5) {
+// 	width: ${this.getWidth(4)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+6):first-child ~ li:nth-child(n+2):nth-child(-n+6) {
+// 	width: ${this.getWidth(5)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+7):first-child ~ li:nth-child(n+2):nth-child(-n+7) {
+// 	width: ${this.getWidth(6)};
+// }`;
+// 		}
+// 		if ( mod === 9 ){
+// 			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+// //			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+4):nth-child(-n+11) {
+// //	width: ${this.getWidth(8)};
+// //}`;
+// 			query+= `\nli:nth-last-child(${mod}n+3):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+4):first-child ~ li:nth-child(n+2):nth-child(-n+4) {
+// 	width: ${this.getWidth(3)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+5):first-child ~ li:nth-child(n+2):nth-child(-n+5) {
+// 	width: ${this.getWidth(4)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+6):first-child ~ li:nth-child(n+2):nth-child(-n+6) {
+// 	width: ${this.getWidth(5)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+7):first-child ~ li:nth-child(n+2):nth-child(-n+7) {
+// 	width: ${this.getWidth(6)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+8):first-child ~ li:nth-child(n+2):nth-child(-n+8) {
+// 	width: ${this.getWidth(7)};
+// }`;
+// 		}
+// 		if ( mod === 10 ){
+// 			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+// //			query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+4):nth-child(-n+12) {
+// //	width: ${this.getWidth(9)};
+// //}`;
+// 			query+= `\nli:nth-last-child(${mod}n+3):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+// 	width: ${this.getWidth(2)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+4):first-child ~ li:nth-child(n+2):nth-child(-n+4) {
+// 	width: ${this.getWidth(3)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+5):first-child ~ li:nth-child(n+2):nth-child(-n+5) {
+// 	width: ${this.getWidth(4)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+6):first-child ~ li:nth-child(n+2):nth-child(-n+6) {
+// 	width: ${this.getWidth(5)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+7):first-child ~ li:nth-child(n+2):nth-child(-n+7) {
+// 	width: ${this.getWidth(6)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+8):first-child ~ li:nth-child(n+2):nth-child(-n+8) {
+// 	width: ${this.getWidth(7)};
+// }`;
+// 			query+= `\nli:nth-last-child(${mod}n+9):first-child ~ li:nth-child(n+2):nth-child(-n+9) {
+// 	width: ${this.getWidth(8)};
+// }`;
+// 		}
+		return query;
+	},
+	componentDidUpdate: function() {
+		hljs.initHighlighting.called = false;
+		hljs.initHighlighting();
+	},
 	render() {
+		// let mod = this.props.mod;
+		// this.state.items = Array.apply(null, Array(mod)).map(function () {});
+		//	newArray = Array.apply(null, Array(mod)).map(function () {});
 		let itemObjects = this.state.items.map(function(item) {
 			return <li className="item"></li>
-		});
+		}),
+			styles = function(m, _this){
+				let mod = parseInt(m),
+					getWidth = function(i){ return Math.round(100000/i)/1000+"%" },
+					width = Math.round(100000/mod)/1000,
+					styles,
+					i = mod-1,
+					query='',
+					cssCommon = _this.makeCommonCSS(mod),
+					cssRow = _this.makeSecondRowCSS(mod),
+					cssMod = _this.makeModCSS(mod),
+					common = `/* mod ${mod} */
+	li {
+		width: ${getWidth(mod)};
+	}
+	li:first-child {
+		width:100%;
+	}
+	li:nth-child(2):last-child {
+	  margin-left: 25%;
+	}`;
+
+				query = `li:nth-last-child(${mod}n+0):first-child ~ li:nth-child(n+2):nth-child(-n+${mod}) {
+		width: ${getWidth(mod-1)};
+	}`;
+				if ( mod === 3 ){
+					query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+${mod+2}) {
+		width: ${getWidth(mod-1)};
+	}`;
+				}
+				if ( mod === 4 ){
+					query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+		width: ${getWidth(2)};
+	}`;
+					query+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+4):nth-child(-n+6) {
+		width: ${getWidth(3)};
+	}`;
+					query+= `\nli:nth-last-child(${mod}n+3):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
+		width: ${getWidth(2)};
+	}`;
+				}
+
+
+				styles = common+"\n"+query;
+				// return styles;
+				return cssCommon +"\n"+ cssRow +"\n"+ cssMod;
+		}(this.state.mod, this)
+
 		return (
 			<div>
-				<style>{this.props.styles}</style>
-				<h2>Try it out</h2>
-				<p>Your  query will be reflected on the items below by a change in colour. Add and remove items to see the styling be applied when the query matches.</p>
-				<header className="controls">
-					<div onClick={this.addItem} className="itemClick"><i className="fa fa-plus-circle "></i> Add Item</div>
-					<div onClick={this.removeItem} className="itemClick"><i className="fa fa-minus-circle"></i> Remove Item</div>
-				</header>
-				<section className="itemList">
+				<section>
+					<p>Select the mod base for your list</p>
+					<label htmlFor="element">Mod {this.state.mod}</label>
+					<input data-tag={this.state.mod}
+						   type="range"
+						   step="1"
+						   min="3"
+						   max="10"
+						   onChange={this.updateMod}
+						   value={this.state.mod}
+						   autoFocus  />
+					<div>
+						<button onClick={this.addItem} className="btn btn-default"><i className="fa fa-plus-circle "></i> Add Item</button>
+						<button onClick={this.removeItem} className="btn btn-default"><i className="fa fa-minus-circle"></i> Remove Item</button>
+					</div>
+				</section>
+				<section>
+					<style>{styles}</style>
 					<ul>
 						{itemObjects}
 					</ul>
+				</section>
+				<section>
+					<pre><code class="css">{styles}</code></pre>
 				</section>
 			</div>
 		)
 	}
 });
 
-const Display = React.createClass({
+const Appold = React.createClass({
+	getInitialState() {
+		return {
+			data: {
+				mod: 3
+			}
+		}
+	},
 	render() {
-		let m = this.props.data.mod || 3,
-			mod = parseInt(m),
-			getWidth = function(i){ return Math.round(100000/i)/1000+"%" },
-			width = Math.round(100000/mod)/1000,
-			styles,
-			i = mod-1,
-			equation='',
-			common = `/* mod ${mod} */
-li {
-	width: ${getWidth(mod)};
-}
-li:first-child {
-	width:100%;
-}
-li:nth-child(2):last-child {
-  margin-left: 25%;
-}`;
-
-			equation = `li:nth-last-child(${mod}n+0):first-child ~ li:nth-child(n+2):nth-child(-n+${mod}) {
-	width: ${getWidth(mod-1)};
-}`;
-			if ( mod === 3 ){
-				equation+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+${mod+2}) {
-	width: ${getWidth(mod-1)};
-}`;
-			}
-			if ( mod === 4 ){
-				equation+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
-	width: ${getWidth(2)};
-}`;
-				equation+= `\nli:nth-last-child(${mod}n+2):first-child ~ li:nth-child(n+4):nth-child(-n+6) {
-	width: ${getWidth(3)};
-}`;
-				equation+= `\nli:nth-last-child(${mod}n+3):first-child ~ li:nth-child(n+2):nth-child(-n+3) {
-	width: ${getWidth(2)};
-}`;
-			}
-
-
-		styles = common+"\n"+equation;
 		return (
-			<div className="displayBody">
+			<div>
 				<Example styles={styles} />
-				<section className="equationDisplay">
-					<h2>Your Code</h2>
-					<p>Copy and paste the code below into your styles</p>
-					<pre><code>{styles}</code></pre>
-				</section>
+
 			</div>
 		);
 	}
 });
 
-const App = React.createClass({
-	getInitialState() {
-		return {
-			data: {
-				mod: ''
-			}
-		}
-	},
-	onUpdate(val){
-		console.log(val);
-		this.setState({
-			data: {
-				mod: val
-			}
-		});
-	},
-	render() {
-		return (
-			<section className="appBody">
-				<Form onUpdate={this.onUpdate} data={this.state.data} />
-				<Display data={this.state.data} />
-			</section>
-		);
-	}
-});
 
 $(document).ready(function() {
-	React.render(<App />, document.querySelector('#content'));
-	$(document).on('click', 'p.explain', function() {
-		$('#lightbox').addClass('show');
-	});
-	$(document).on('click', '.closeButton', function() {
-		$('#lightbox').removeClass('show');
-	});
-	$(document).keyup(function(e) {
-	     if (e.keyCode == 27) { // escape key maps to keycode `27`
-	       $('#lightbox').removeClass('show');
-	    }
-	});
+	React.render(<App />, document.querySelector('article'));
+	hljs.initHighlightingOnLoad();
 });
